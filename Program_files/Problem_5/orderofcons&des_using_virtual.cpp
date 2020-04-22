841b0c30fd0dfd1922322ea68a1cc8f806c4d9a8
Rayapati-SaI-Krishna <63155622+Rayapati-SaI-Krishna@users.noreply.github.com>
2020-04-23 01:34:55 +0530

    Update orderofcons&des_using_virtual.cpp

--- Program_files/Problem_5/orderofcons&des_using_virtual.cpp
+++ Program_files/Problem_5/orderofcons&des_using_virtual.cpp
@@ -1,52 +1,41 @@
-#include<iostream>
+#include <iostream> 
 #include<string.h>
 using namespace std;
-
-
-class A //class creation
+ 
+class Container
 {
 public:
-    A() {
-       
-    }
-
-    virtual void virtual_function()//virtual function {
-        cout<<"A::virtual_function called"<<endl;
-    }
-
-    virtual ~A()//virtual destructor 
+    Container() {};
+    virtual ~Container()
     {
-    }
+        cout << "Base class destructor called - not much to do." << endl;
+    };
 };
-
-class B : public A 
-{
+ 
+class CardboardBox: public Container {
 public:
-    // override virtual function in A
-    void virtual_function()
+    CardboardBox() {};
+    ~CardboardBox()
     {
-        cout<<"B::virtual_function called"<<endl;
-    }
+        cout << "Sub class destructor called - let's recycle the cardboard!" << endl;
+    };
 };
-
-int main(int argc, char **argv)
+ 
+int main(int argc,char **argv)
 {
-    
-     
-    if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
+      if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
    
     {
         cout<<"The main purpose of this program is to how to use & Declare the Local & Global scope variable"<<endl;
     }
-    else{
-    // Call to virtual_function during construction doesn't
-    // behave like normal virtual function call.
-    // Print statement shows it invokes A::virtual_function,
-    // even though we are constructing an instance of B, not A.
-     B myObject;
-
-    // This call behaves like a normal virtual function call.
-    // Print statement shows it invokes B::virtual_function.
-    myObject.virtual_function();
-}
+    else
+    {
+    CardboardBox* box = new CardboardBox;
+ 
+    Container* p = box;
+ 
+    delete p;
+    }
+ 
+    return 0;
 }

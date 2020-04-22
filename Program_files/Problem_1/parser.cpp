b216a07c4c50002e0036eb08126493f3f3df7293
Rayapati-SaI-Krishna <63155622+Rayapati-SaI-Krishna@users.noreply.github.com>
2020-04-22 23:30:46 +0530

    Update parser.cpp

--- Program_files/Problem_1/parser.cpp
+++ Program_files/Problem_1/parser.cpp
@@ -1,40 +1,41 @@
-#include <iostream> 
-#include<string.h>
-#include<stdlib.h>
-#include<cmath>
-using namespace std; 
-int main(int argc, char** argv) 
-{ 
-    
-     if(argc == 2 && strcmp(argv[1], "-h")==0) //help command 
-   
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+int main(int argc, char **argv)
+{
+
+    if (argc == 2 && strcmp(argv[1], "-h") == 0) //help command
+
     {
-        cout<<"The main purpose of this program is to how to use & Declare Different way to initialize the private member variables in a class"<<endl;
+        cout << "The main purpose of this program is to how to know the size of given input" << endl;
     }
-    else {
-	for (int i = 1; i < argc; ++i) {
-		cout << argv[i]<<' ' ;
-		
-    
-   if(((strlen(argv[i]))==1) && (*argv[i]>='a'&& *argv[i]<='z') || (*argv[i]>='A' && *argv[i]<='Z'))
-   {
-       cout<<sizeof(char);
-   }
-   else if((strlen(argv[i]))!=1)
-   {
-       cout<<sizeof(string);
-   }
-    
-   else if(atoi(argv[i])>=0 && atoi(argv[i])<=9)
-   {
-       cout<<sizeof(int);
-   }
-   else if((atoi(argv[i]))==(atof(argv[i])) )
-   {
-       cout<<sizeof(double);
-   }
-   cout << "\n" ;
-	}
-	return 0; 
-}
-} 
+    else
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            cout << argv[i] << ' ';
+
+            
+            if (((strlen(argv[i])) != 1) && (atof(argv[i]))==0)
+            {
+                cout << " String "<< sizeof(string);
+            }
+            else if (((strlen(argv[i])) == 1) && (*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' && *argv[i] <= 'Z'))
+            {
+                cout << " Char " <<sizeof(char);
+            }
+            else if (floor(atof(argv[i]))-(atof(argv[i]))!=0)
+            {
+                cout << " Double " <<sizeof(double);
+            }
+            
+            else if (atoi(argv[i]) >= 0 )
+            {
+                cout << " Integer " << sizeof(int);
+            }
+            cout << "\n";
+        }
+        return 0;
+    }
